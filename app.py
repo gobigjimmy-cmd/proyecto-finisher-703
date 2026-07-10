64005ce5bdcee8f706d1576636acd566126a5824
gobigjimmy-cmd <gobig.jimmy@gmail.com>
2026-07-10 16:17:05 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Proyecto FINISHER 70.3 - Panamá 2027")
+st.subheader("Atleta: Jimmy Peña")
+st.write("El motor de tu preparación está encendiéndose. ¡Bienvenido a bordo!")
